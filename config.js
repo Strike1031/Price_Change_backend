82939aa20ee62f7ffe1f306e74f497ffaca28ff8
Strike <strike10310522@gmail.com>
2022-11-01 06:16:46 -0700

    reduce 4 functions to one
    -reduce 4 functions to one function with parameters

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ import pairABI from './components/ABI/pairABI.js';
 import routerABI from './components/ABI/routerABI.js';
 
 const data = {
-    mainDB: 'mongodb://admin:MasterDBDEXNodeAdm1n2022@51.195.60.1:27017/BSC?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
+    // mainDB: 'mongodb://admin:MasterDBDEXNodeAdm1n2022@51.195.60.1:27017/BSC?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
+    mainDB: 'mongodb://admin:DMCScr4perAdm1nDBase@146.190.208.102:27017/BSC?authSource=admin&readPreference=primary&directConnection=true&ssl=false',
     localDB: 'mongodb://127.0.0.1:27017/Dexmarketcap',
     rpc_BSC: 'http://51.77.68.21:8545',
     rpc_ETH: 'http://135.125.188.224:8545',
--- controllers/pcs.js
+++ controllers/pcs.js
@@ -15,38 +15,31 @@ export async function monitorChange() {
   //cron jobs--token change prices It is running every second
   cron.schedule("*/1 * * * * *", async () => {
     //console.log('running every 5 seconds:');
-    if (timer == 0)  change(pairs);
+    if (timer == 0) change(pairs);
   });
 }
 
 async function change(pairs) {
   timer = 1;
- // console.log("change--{function} started!");
+  // console.log("change--{function} started!");
   for (let i = 0; i < pairs.length; i++) {
     await priceChange(pairs[i]).then((data) => {
       console.log("---------------------");
       console.log(data);
       console.log("---------------------");
     });
-    if (i ==  pairs.length-1) timer = 0;
+    if (i == pairs.length - 1) timer = 0;
   }
-  //   pairs.map((address) => {
-  //     priceChange(address).then((data) => {
-  //       console.log("---------------------");
-  //       console.log(data);
-  //       console.log("---------------------");
-  //     });
-  //  });
 }
 
+var m_monitorTimes = [86400, 300, 3600, 21600];
 /**
  *
  * @param {string} address
  * @returns aggregated pair info
  */
 async function priceChange(address) {
-  const currentTime = parseInt((Date.now() / 1000).toFixed(0));
-  let result = {
+  var result = {
     token0Symbol: "",
     token1Symbol: "",
     price1: "$0",
@@ -60,15 +53,23 @@ async function priceChange(address) {
     liquidity: "0",
   };
 
+  for (let i = 0; i < m_monitorTimes.length; i++) {
+    await eachPriceChange(address, m_monitorTimes[i], result);
+  }
+  return result;
+}
+
+async function eachPriceChange(address, monitorTime, result) {
+  const currentTime = parseInt((Date.now() / 1000).toFixed(0));
   /**
-   *   24h_PriceChange Aggregation
+   *  Each time PriceChange Aggregation
    */
   let res = await tx.aggregate([
     {
       $match: {
         $and: [
           { pairAddress: address },
-          { timestamp: { $gte: currentTime - 86400 } },
+          { timestamp: { $gte: currentTime - monitorTime } },
         ],
       },
     },
@@ -100,192 +101,67 @@ async function priceChange(address) {
       },
     },
   ]);
-  const {
-    reserves0Close,
-    reserves1Close,
-    reserves0Open,
-    reserves1Open,
-    amount0,
-    amount1,
-    pairAddress,
-    token0,
-    token1,
-    token0Symbol,
-    token1Symbol,
-    dex,
-    network,
-  } = res[0];
 
-  const price0Open = reserves0Open / reserves1Open;
-  const price0Close = reserves0Close / reserves1Close;
-
-  const price1Open = reserves1Open / reserves0Open;
-  const price1Close = reserves1Close / reserves0Close;
-
-  const percDiff0 = (
-    100 * Math.abs((price0Open - price0Close) / (price0Open + price0Close) / 2)
-  ).toFixed(2);
-  const percDiff1 = (
-    100 * Math.abs((price1Open - price1Close) / (price1Open + price1Close) / 2)
-  ).toFixed(2);
-
-  const supply0 = await getSupply(token0, network);
-  const supply1 = await getSupply(token1, network);
-
-  const decimal0 = await getDecimals(token0, network);
-  const decimal1 = await getDecimals(token1, network);
-
-  result.token0Symbol = token0Symbol;
-  result.token1Symbol = token1Symbol;
-  result.price1 = "$" + price1Close;
-  result.priceChange_24h =
-    price1Open > price1Close ? `-${percDiff1}` : `+${percDiff1}`;
-  result.volume = amount1 + "";
-  result.marketCap = "";
-  result.txns = "";
-  result.liquidity = "";
-
-//  console.log("5m_PriceChange");
-  /**
-   *   5m_PriceChange Aggregation
-   */
-
-  let res1 = await tx.aggregate([
-    {
-      $match: {
-        $and: [
-          { pairAddress: address },
-          { timestamp: { $gte: currentTime - 300 } },
-        ],
-      },
-    },
-
-    {
-      $group: {
-        _id: "$pairAddress",
-        reserves0Open_5m: { $first: "$reserves0" },
-        reserves1Open_5m: { $first: "$reserves1" },
-        reserves0Close_5m: { $last: "$reserves0" },
-        reserves1Close_5m: { $last: "$reserves1" },
-      },
-    },
-  ]);
-
-  if (res1.length != 0) {
+  if (res.length > 0) {
     const {
-      reserves1Close_5m,
-      reserves1Open_5m,
-      reserves0Open_5m,
-      reserves0Close_5m,
-    } = res1[0];
+      reserves0Close,
+      reserves1Close,
+      reserves0Open,
+      reserves1Open,
+      amount0,
+      amount1,
+      pairAddress,
+      token0,
+      token1,
+      token0Symbol,
+      token1Symbol,
+      dex,
+      network,
+    } = res[0];
 
-    const price1Open_5m = reserves1Open_5m / reserves0Open_5m;
-    const price1Close_5m = reserves1Close_5m / reserves0Close_5m;
+    const price0Open = reserves0Open / reserves1Open;
+    const price0Close = reserves0Close / reserves1Close;
 
-    const percDiff1_5m = (
+    const price1Open = reserves1Open / reserves0Open;
+    const price1Close = reserves1Close / reserves0Close;
+
+    const percDiff0 = (
       100 *
-      Math.abs(
-        (price1Open_5m - price1Close_5m) / (price1Open_5m + price1Close_5m) / 2
-      )
+      Math.abs((price0Open - price0Close) / (price0Open + price0Close) / 2)
+    ).toFixed(2);
+    const percDiff1 = (
+      100 *
+      Math.abs((price1Open - price1Close) / (price1Open + price1Close) / 2)
     ).toFixed(2);
 
-    result.priceChange_5m =
-      price1Open_5m > price1Close_5m ? `-${percDiff1_5m}` : `+${percDiff1_5m}`;
+    if (monitorTime == 86400) {
+      const supply0 = await getSupply(token0, network);
+      const supply1 = await getSupply(token1, network);
+
+      const decimal0 = await getDecimals(token0, network);
+      const decimal1 = await getDecimals(token1, network);
+
+      result.token0Symbol = token0Symbol;
+      result.token1Symbol = token1Symbol;
+      result.price1 = "$" + price1Close;
+      result.priceChange_24h =
+        price1Open > price1Close ? `-${percDiff1}` : `+${percDiff1}`;
+      result.volume = amount1 + "";
+      result.marketCap = "";
+      result.txns = "";
+      result.liquidity = "";
+    } else {
+      switch(monitorTime) {
+        case 300:
+          result.priceChange_5m = price1Open > price1Close ? `-${percDiff1}` : `+${percDiff1}`;
+          break;
+        case 3600:
+          result.priceChange_1h = price1Open > price1Close ? `-${percDiff1}` : `+${percDiff1}`;
+          break;
+        case 21600:
+          result.priceChange_6h = price1Open > price1Close ? `-${percDiff1}` : `+${percDiff1}`;
+          break;
+      }
+    }
   }
-
-//  console.log("1h_PriceChange");
-  /**
-   *   1h_PriceChange Aggregation
-   */
-  let res2 = await tx.aggregate([
-    {
-      $match: {
-        $and: [
-          { pairAddress: address },
-          { timestamp: { $gte: currentTime - 3600 } },
-        ],
-      },
-    },
-
-    {
-      $group: {
-        _id: "$pairAddress",
-        reserves0Open_1h: { $first: "$reserves0" },
-        reserves1Open_1h: { $first: "$reserves1" },
-        reserves0Close_1h: { $last: "$reserves0" },
-        reserves1Close_1h: { $last: "$reserves1" },
-      },
-    },
-  ]);
-
-  if (res2.length != 0) {
-    const {
-      reserves1Close_1h,
-      reserves1Open_1h,
-      reserves0Open_1h,
-      reserves0Close_1h,
-    } = res2[0];
-
-    const price1Open_1h = reserves1Open_1h / reserves0Open_1h;
-    const price1Close_1h = reserves1Close_1h / reserves0Close_1h;
-
-    const percDiff1_1h = (
-      100 *
-      Math.abs(
-        (price1Open_1h - price1Close_1h) / (price1Open_1h + price1Close_1h) / 2
-      )
-    ).toFixed(2);
-
-    result.priceChange_1h =
-      price1Open_1h > price1Close_1h ? `-${percDiff1_1h}` : `+${percDiff1_1h}`;
-  }
-
- // console.log("6h_PriceChange");
-  /**
-   *   6h_PriceChange Aggregation
-   */
-  let res3 = await tx.aggregate([
-    {
-      $match: {
-        $and: [
-          { pairAddress: address },
-          { timestamp: { $gte: currentTime - 21600 } },
-        ],
-      },
-    },
-
-    {
-      $group: {
-        _id: "$pairAddress",
-        reserves0Open_6h: { $first: "$reserves0" },
-        reserves1Open_6h: { $first: "$reserves1" },
-        reserves0Close_6h: { $last: "$reserves0" },
-        reserves1Close_6h: { $last: "$reserves1" },
-      },
-    },
-  ]);
-
-  if (res3.length != 0) {
-    const {
-      reserves1Close_6h,
-      reserves1Open_6h,
-      reserves0Open_6h,
-      reserves0Close_6h,
-    } = res3[0];
-
-    const price1Open_6h = reserves1Open_6h / reserves0Open_6h;
-    const price1Close_6h = reserves1Close_6h / reserves0Close_6h;
-
-    const percDiff1_6h = (
-      100 *
-      Math.abs(
-        (price1Open_6h - price1Close_6h) / (price1Open_6h + price1Close_6h) / 2
-      )
-    ).toFixed(2);
-
-    result.priceChange_6h =
-      price1Open_6h > price1Close_6h ? `-${percDiff1_6h}` : `+${percDiff1_6h}`;
-  }
-
-  return result;
 }
